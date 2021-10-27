ab4eac167d230696360278898e3441f8479cb152
Suibin Hong <suibin.hong@outlook.com>
2021-10-27 10:32:57 +0200

    Updated next.js to 12.0 and now search functionality will be implemented

--- next.config.js
+++ next.config.js
@@ -5,5 +5,6 @@ module.exports = {
     domains: ['images.unsplash.com', 'image.tmdb.org', 'critics.io', 'www.seekpng.com', 'avatars.dicebear.com'],
   }, env: {
     NEXT_MOVIE_DB_API_KEY: process.env.NEXT_MOVIE_DB_API_KEY,
-  }
+  },
+  swcMinify: true
 }
